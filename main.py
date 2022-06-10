bb017c13fcf41e2d9409b9df815d3e02af48fc2b
jitesh1059 <jiteshmoganaraja@gmail.com>
2022-06-10 12:26:09 +0000

    Update for reddit

--- main.py
+++ main.py
@@ -20,34 +20,63 @@ def reddit_object():
     print(reddit.user.me())
 
     url = str(input("Please input the reddit URL: "))
-    substring = "comments"
+    # substring = "comments"
 
-    if url.find(substring) != -1:
-        submission = reddit.submission(url=url)
-        try:
-            content["thread_url"] = submission.url
-            content["thread_title"] = submission.title
-            content["thread_post"] = submission.selftext
-            content["comments"] = []
+    # if url.find(substring) != -1:
+    #     submission = reddit.submission(url=url)
+    #     try:
+    #         content["thread_url"] = submission.url
+    #         content["thread_title"] = submission.title
+    #         content["thread_post"] = submission.selftext
+    #         content["comments"] = []
 
-            for top_level_comment in submission.comments:
-                if not top_level_comment.stickied:
-                    content["comments"].append(
-                        {
-                            "comment_body": top_level_comment.body,
-                            "comment_url": top_level_comment.permalink,
-                            "comment_id": top_level_comment.id,
-                        }
-                    )
+    #         for top_level_comment in submission.comments:
+    #             if not top_level_comment.stickied:
+    #                 content["comments"].append(
+    #                     {
+    #                         "comment_body": top_level_comment.body,
+    #                         "comment_url": top_level_comment.permalink,
+    #                         "comment_id": top_level_comment.id,
+    #                     }
+    #                 )
 
-        except AttributeError as e:
-            pass
+    #     except AttributeError as e:
+    #         pass
 
-        print("Received AskReddit threads successfully.")
+    #     print("Received AskReddit threads successfully.")
+
+    # else:
+    buggy_name = url.split("/r/",1)[1]
+    import requests
     
-    else:
-        x = requests.get(url)
-        print(x.text)
+    # note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
+    auth = requests.auth.HTTPBasicAuth('8jrXecmLwkW93TuWPOJP6w', 'EoUz2jn4WW2o23T56QqQ9-382nbEoA')
+
+    # here we pass our login method (password), username, and password
+    data = {'grant_type': 'password',
+            'username': 'AudioDuck1059',
+            'password': 'Jitesh2910'}
+
+    # setup our header info, which gives reddit a brief description of our app
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+
+    # send our request for an OAuth token
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+                        auth=auth, data=data, headers=headers)
+
+    # convert response to JSON and pull access_token value
+    TOKEN = res.json()['access_token']
+
+    # add authorization to our headers dictionary
+    headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+
+    # while the token is valid (~2 hours) we just add headers=headers to our requests
+    requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
+
+    res = requests.get(f"https://oauth.reddit.com/r/{buggy_name}hot",
+                   headers=headers)
+
+    print(res.json())  # let's see what we get
 
 
 def save_text_to_mp3(reddit_obj):
@@ -87,5 +116,6 @@ def save_text_to_mp3(reddit_obj):
             f"assets/mp3/{reddit_obj['thread_title']}{idx}.mp3").info.length
 
     return idx, length
+
 
 reddit_object()
