839ace6ffb9f12a0f3575e03da5a941a59a64014
jitesh1059 <jiteshmoganaraja@gmail.com>
2022-06-18 07:06:39 +0000

    New Things

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ from pytube import YouTube
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.editor import VideoFileClip
 import re
+from os import listdir, environ
+from moviepy.video.io import ffmpeg_tools
 
 theme = "dark"
 W, H = 1080, 1920
@@ -96,9 +98,6 @@ def reddit_object():
 
         res = requests.get(f"https://oauth.reddit.com/r/{buggy_name}new",
                     headers=headers, params = {"limit": num})
-
-        item = res.json()['data']['children'][0]
-        print(item['data']['id'])
         
         try:
             content["thread_title"] = f"{name}"
@@ -255,11 +254,7 @@ def download_background():
     """Downloads the backgrounds/s video from YouTube."""
     Path("./assets/backgrounds/").mkdir(parents=True, exist_ok=True)
     background_options = [  # uri , filename , credit
-        (
-            "https://www.youtube.com/watch?v=2X9QGY__0II",
-            "rocket_league.mp4",
-            "Orbital Gameplay",
-        ),
+        ("https://www.youtube.com/watch?v=n_Dv4JMiwK8", "parkour.mp4", "bbswitzer"),
     ]
     # note: make sure the file name doesn't include an - in it
     if not len(listdir("./assets/backgrounds")) >= len(
@@ -284,14 +279,12 @@ def download_background():
 
 def chop_background_video(video_length):
     print("Finding a spot in the backgrounds video to chop...✂️")
-    choice = random.choice(listdir("assets/backgrounds"))
-    environ["background_credit"] = choice.split("-")[0]
 
-    background = VideoFileClip(f"assets/backgrounds/{choice}")
+    background = VideoFileClip(f"assets/backgrounds/bbswitzer-parkour.mp4")
 
     start_time, end_time = get_start_and_end_times(video_length, background.duration)
     ffmpeg_extract_subclip(
-        f"assets/backgrounds/{choice}",
+        f"assets/backgrounds/bbswitzer-parkour.mp4",
         start_time,
         end_time,
         targetname="assets/temp/background.mp4",
@@ -299,15 +292,13 @@ def chop_background_video(video_length):
     print("Background video chopped successfully!")
     return True
 
-def make_final_video(number_of_clips, name, url):
-    
-    print("Creating the final video...")
 
+def make_final_video(number_of_clips, length, name, url, new_obj):
+    print_step("Creating the final video 🎥")
     VideoFileClip.reW = lambda clip: clip.resize(width=W)
     VideoFileClip.reH = lambda clip: clip.resize(width=H)
-
     background_clip = (
-        VideoFileClip("assets/mp4/clip.mp4")
+        VideoFileClip("assets/temp/background.mp4")
         .without_audio()
         .resize(height=H)
         .crop(x1=1166.6, y1=0, x2=2246.6, y2=1920)
@@ -316,59 +307,119 @@ def make_final_video(number_of_clips, name, url):
     # Gather all audio clips
     audio_clips = []
     for i in range(0, number_of_clips):
-        audio_clips.append(AudioFileClip(f"assets/mp3/{i}.mp3"))
-    audio_clips.insert(0, AudioFileClip(f"assets/mp3/title.mp3"))
-    try:
-        audio_clips.insert(1, AudioFileClip(f"assets/mp3/posttext.mp3"))
-    except:
-        OSError()
+        audio_clips.append(AudioFileClip(f"assets/temp/mp3/{i}.mp3"))
+    audio_clips.insert(0, AudioFileClip(f"assets/temp/mp3/title.mp3"))
     audio_concat = concatenate_audioclips(audio_clips)
     audio_composite = CompositeAudioClip([audio_concat])
 
-    # Gather all images
+    # Get sum of all clip lengths
+    total_length = sum([clip.duration for clip in audio_clips])
+    # round total_length to an integer
+    int_total_length = round(total_length)
+    # Output Length
+    console.log(f"[bold green] Video Will Be: {int_total_length} Seconds Long")
+
+    # add title to video
     image_clips = []
-    for i in range(0, number_of_clips):
-        image_clips.append(
-            ImageClip(f"assets/png/comment_{i}.png")
-            .set_duration(audio_clips[i + 1].duration)
-            .set_position("center")
-            .resize(width=W - 100)
-            .set_opacity(float(opacity)),
-        )
-    if os.path.exists(f"assets/mp3/posttext.mp3"):
+    # Gather all images
+    if (
+            opacity is None or float(opacity) >= 1
+    ):  # opacity not set or is set to one OR MORE
         image_clips.insert(
             0,
-            ImageClip(f"assets/png/title.png")
-            .set_duration(audio_clips[0].duration + audio_clips[1].duration)
-            .set_position("center")
-            .resize(width=W - 100)
-            .set_opacity(float(opacity)),
-            )
-    else:
-        image_clips.insert(
-            0,
-            ImageClip(f"assets/png/title.png")
+            ImageClip(f"assets/temp/png/title.png")
             .set_duration(audio_clips[0].duration)
             .set_position("center")
             .resize(width=W - 100)
             .set_opacity(float(opacity)),
+        )
+    else:
+        image_clips.insert(
+            0,
+            ImageClip(f"assets/temp/png/title.png")
+            .set_duration(audio_clips[0].duration)
+            .set_position("center")
+            .resize(width=W - 100))
+
+    for i in range(0, number_of_clips):
+        if (
+                opacity is None or float(opacity) >= 1
+        ):  # opacity not set or is set to one OR MORE
+            image_clips.append(
+                ImageClip(f"assets/temp/png/comment_{i}.png")
+                .set_duration(audio_clips[i + 1].duration)
+                .set_position("center")
+                .resize(width=W - 100),
             )
+        else:
+            image_clips.append(
+                ImageClip(f"assets/temp/png/comment_{i}.png")
+                .set_duration(audio_clips[i + 1].duration)
+                .set_position("center")
+                .resize(width=W - 100)
+                .set_opacity(float(opacity)),
+            )
+
+    # if os.path.exists("assets/mp3/posttext.mp3"):
+    #    image_clips.insert(
+    #        0,
+    #        ImageClip("assets/png/title.png")
+    #        .set_duration(audio_clips[0].duration + audio_clips[1].duration)
+    #        .set_position("center")
+    #        .resize(width=W - 100)
+    #        .set_opacity(float(opacity)),
+    #    )
+    # else:
     image_concat = concatenate_videoclips(image_clips).set_position(
         ("center", "center")
     )
     image_concat.audio = audio_composite
     final = CompositeVideoClip([background_clip, image_concat])
-    substring = "comments"
-    if url.find(substring) != -1:
-        filename = (re.sub('[?\"%*:|<>]', '', ("assets/" + reddit.subreddit.submission.title + ".mp4")))
-        final.write_videofile(filename, fps=30, audio_codec="aac", audio_bitrate="192k")
-        for i in range(0, number_of_clips):
-            pass
-    else:
-        filename = (re.sub('[?\"%*:|<>]', '', ("assets/" + name + ".mp4")))
-        final.write_videofile(filename, fps=30, audio_codec="aac", audio_bitrate="192k")
-        for i in range(0, number_of_clips):
-            pass
+
+    def get_video_title() -> str:
+        title = name
+        if len(title) <= 35:
+            return title
+        else:
+            return title[0:30] + "..."
+
+    filename = f"{get_video_title()}.mp4"
+
+    def save_data():
+        with open("./video_creation/data/videos.json", "r+") as raw_vids:
+            done_vids = json.load(raw_vids)
+            if str(subreddit.submission.id) in [video["id"] for video in done_vids]:
+                return  # video already done but was specified to continue anyway in the .env file
+            payload = {
+                "time": str(int(time.time())),
+                "reddit_title": str(name),
+                "filename": filename
+            }
+            done_vids.append(payload)
+            raw_vids.seek(0)
+            json.dump(done_vids, raw_vids, ensure_ascii=False, indent=4)
+
+    save_data()
+    if not exists("./results"):
+        print_substep("the results folder didn't exist so I made it")
+        os.mkdir("./results")
+
+    final.write_videofile(
+        "assets/temp/temp.mp4", fps=30, audio_codec="aac", audio_bitrate="192k"
+    )
+    ffmpeg_tools.ffmpeg_extract_subclip(
+        "assets/temp/temp.mp4", 0, length, targetname=f"results/{filename}"
+    )
+    # os.remove("assets/temp/temp.mp4")
+
+    print_step("Removing temporary files 🗑")
+    cleanups = cleanup()
+    print_substep(f"Removed {cleanups} temporary files 🗑")
+    print_substep(f"See result in the results folder!")
+
+    print_step(
+        f"Reddit title: {name}"
+    )
 
 banner = """
 ██████╗ ███████╗██████╗ ██████╗ ██╗████████╗    ██╗   ██╗██╗██████╗ ███████╗ ██████╗     ███╗   ███╗ █████╗ ██╗  ██╗███████╗██████╗ 
@@ -386,4 +437,4 @@ download_screenshots_of_reddit_posts(
 )
 download_background()
 chop_background_video(length)
-make_final_video(number_of_comments, name, url)
+make_final_video(number_of_comments, length, name, url, new_obj)
